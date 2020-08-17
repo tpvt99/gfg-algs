afaaf6bc62161b167f1b1cd1cc76a047667bab6d
tpvt96 <tranphong96.hbk@gmail.com>
2020-08-17 20:22:36 +0800

    general counting sort

--- Chapter9.Sorting/CoutingSort/CoutingSort/CoutingSort.cpp
+++ Chapter9.Sorting/CoutingSort/CoutingSort/CoutingSort.cpp
@@ -2,16 +2,27 @@
 //
 
 #include <iostream>
+#include <algorithm>
 
 void countingSort(int* a, int n, int k);
+void generalCountingSort(int* a, int n, int k);
+void generalCountingSortWithAnyK(int* a, int n, int k, int min);
 
 int main()
 {
     int a[] = { 1, 4, 4, 1, 0, 1, 2, 3, 4, 1, 2, 4 };
-    countingSort(a, sizeof(a) / sizeof(a[0]), 5);
+    generalCountingSort(a, sizeof(a) / sizeof(a[0]), 5);
     printf("Array after sort: ");
     for (int i = 0;i < sizeof(a) / sizeof(a[0]);i++) {
         printf("%d ", a[i]);
+    }
+    printf("\n");
+    int b[] = { -12, -3, -4,21, 32, 43, 12, 1, 3, 4, 5, 6, 1,2, 3,23,-1,02 };
+    int k = *std::max_element(b, b + sizeof(b) / sizeof(b[0])) - *std::min_element(b, b + sizeof(b) / sizeof(b[0]))+1;
+    generalCountingSortWithAnyK(b, sizeof(b) / sizeof(b[0]), k, *std::min_element(b, b + sizeof(b) / sizeof(b[0])));
+    printf("Array after sort: ");
+    for (int i = 0;i < sizeof(b) / sizeof(b[0]);i++) {
+        printf("%d ", b[i]);
     }
 }
 
@@ -32,4 +43,63 @@ void countingSort(int* a, int n, int k) {
             a[counter++] = i;
         }
     }
+}
+
+void generalCountingSort(int* a, int n, int k) {
+    int* counts = new int[k];
+    // Step 1. Initialize 0s
+    for (int i = 0; i < k;i++) {
+        counts[i] = 0;
+    }
+    // Step 2. Counts frequencies
+    for (int i = 0; i < n;i++) {
+        counts[a[i]]++;
+    }
+    // Step 3. Accumulate frequencies less than or equal
+    for (int i = 1;i < k;i++) {
+        counts[i] = counts[i - 1] + counts[i];
+    }
+    // Step 4. Iterate backwards
+    int* outputs = new int[n];
+    for (int i = n - 1; i >= 0;i--) {
+        int frequencies = counts[a[i]];
+        outputs[frequencies - 1] = a[i];
+        counts[a[i]] -= 1;
+    }
+    // Step 5. Copy back outputs[] to a[]
+    for (int i = 0; i < n;i++) {
+        a[i] = outputs[i];
+    }
+    delete[] outputs;
+    delete[] counts;
+}
+
+void generalCountingSortWithAnyK(int* a, int n, int k, int min) {
+    int* counts = new int[k];
+    // Step 1. Initialize 0s
+    for (int i = 0; i < k;i++) {
+        counts[i] = 0;
+    }
+    // Step 2. Counts frequencies
+    for (int i = 0; i < n;i++) {
+        counts[a[i]-min]++;
+    }
+    // Step 3. Accumulate frequencies less than or equal
+    for (int i = 1;i < k;i++) {
+        counts[i] = counts[i - 1] + counts[i];
+    }
+
+    // Step 4. Iterate backwards
+    int* outputs = new int[n];
+    for (int i = n - 1; i >= 0;i--) {
+        int frequencies = counts[a[i] - min];
+        outputs[frequencies - 1] = a[i];
+        counts[a[i] - min] -= 1;
+    }
+    // Step 5. Copy back outputs[] to a[]
+    for (int i = 0; i < n;i++) {
+        a[i] = outputs[i];
+    }
+    delete[] outputs;
+    delete[] counts;
 }
