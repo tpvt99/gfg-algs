6521007e9f21d9810ce738e0e32e3041f154b63f
tpvt99 <tranphong96.hbk@gmail.com>
2020-10-16 22:13:00 +0800

    Linked list implementation

---
+++ Chapter13.LinkedLists/Implementation/cmake-build-debug/CMakeFiles/Implementation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Implementation.dir/main.cpp.o"
+  "Implementation"
+  "Implementation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Implementation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter13.LinkedLists/Implementation/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+typedef struct Node{
+    int item;
+    struct Node * next;
+} Node;
+
+void addNode(Node **head, int item);
+void printNode(Node *head);
+
+int main() {
+    Node *head = nullptr;
+    addNode(&head, 10);
+    addNode(&head, 20);
+    addNode(&head, 30);
+    printNode(head);
+    return 0;
+}
+
+void addNode(Node **head, int item) {
+    Node *node = static_cast<Node *>(malloc(sizeof(Node)));
+    if (node == nullptr) {
+        std::cout << "Cannot allocate new node\n";
+        return;
+    }
+    node->item = item;
+
+    if (*head == nullptr) {
+        *head = node;
+        node->next = nullptr;
+        return;
+    }
+
+    Node *end;
+    for(end = *head; end->next != nullptr; end=end->next){
+        ;
+    }
+
+    end->next = node;
+    node->next = nullptr;
+}
+
+void printNode(Node *head) {
+    for (Node *top = head; top != nullptr; top = top->next) {
+        printf("%d -> ", top->item);
+    }
+    printf("NULL\n");
+}
+
