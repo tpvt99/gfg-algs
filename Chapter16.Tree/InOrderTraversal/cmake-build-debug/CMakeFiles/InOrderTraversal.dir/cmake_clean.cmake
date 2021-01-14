4f7ab4666083101f8bbde033ad5ce6131e9c97f0
Tran Phong <tranphong96.hbk@gmail.com>
2021-01-14 21:50:50 +0800

    In Order Traversal

---
+++ Chapter16.Tree/InOrderTraversal/cmake-build-debug/CMakeFiles/InOrderTraversal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InOrderTraversal.dir/main.cpp.o"
+  "InOrderTraversal"
+  "InOrderTraversal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InOrderTraversal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
