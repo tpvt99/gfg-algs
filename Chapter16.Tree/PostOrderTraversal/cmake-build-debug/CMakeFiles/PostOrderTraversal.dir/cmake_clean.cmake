2abbd3ceefc2d53e10df331d67ef5f717d19cf09
Tran Phong <tranphong96.hbk@gmail.com>
2021-01-16 21:53:10 +0800

    Post Order Traversal

---
+++ Chapter16.Tree/PostOrderTraversal/cmake-build-debug/CMakeFiles/PostOrderTraversal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PostOrderTraversal.dir/main.cpp.o"
+  "PostOrderTraversal"
+  "PostOrderTraversal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PostOrderTraversal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
