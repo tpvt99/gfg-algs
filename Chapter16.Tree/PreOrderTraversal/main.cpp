40f8b7c17190f962f059f589748c9dc5fcb0b842
Tran Phong <tranphong96.hbk@gmail.com>
2021-01-16 21:46:29 +0800

    Pre Order Traversal

---
+++ Chapter16.Tree/PreOrderTraversal/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Node {
+public:
+    int key{};
+    Node *left;
+    Node *right;
+public:
+    Node(int x) {
+        key = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void preOrderTraversal(Node *root);
+
+int main() {
+    Node *node1 = new Node(10);
+    Node *node2= new Node(20);
+    Node *node3= new Node(30);
+    Node *node4= new Node(40);
+    Node *node5= new Node(50);
+    Node *node6= new Node(60);
+    Node *node7= new Node(70);
+    Node *node8= new Node(80);
+    node1->left = node2;
+    node1->right = node3;
+    node2->left = node4;
+    node2->right = node5;
+    node5->left = node7;
+    node5->right = node8;
+    node3->right = node6;
+
+    preOrderTraversal(node1);
+
+    return 0;
+}
+
+void preOrderTraversal(Node *root) {
+    if (root != nullptr) {
+        std::cout << root->key << " ";
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
