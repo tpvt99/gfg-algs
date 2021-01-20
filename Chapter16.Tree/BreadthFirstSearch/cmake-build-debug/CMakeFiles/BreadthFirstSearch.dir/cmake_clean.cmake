901a5ff60359a8bcbd6cb85a09bc55782a840d0f
Tran Phong <tranphong96.hbk@gmail.com>
2021-01-20 09:25:22 +0800

    breadth first search

---
+++ Chapter16.Tree/BreadthFirstSearch/cmake-build-debug/CMakeFiles/BreadthFirstSearch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BreadthFirstSearch"
+  "BreadthFirstSearch.pdb"
+  "CMakeFiles/BreadthFirstSearch.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BreadthFirstSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
