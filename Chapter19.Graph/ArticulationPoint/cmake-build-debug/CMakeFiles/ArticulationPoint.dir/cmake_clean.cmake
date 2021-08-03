15df44a2511cacbcc4ab0c1b754b7405b9c7fb3e
Tran Phong <tranphong96.hbk@gmail.com>
2021-08-03 23:06:17 +0800

    Articulation Point

---
+++ Chapter19.Graph/ArticulationPoint/cmake-build-debug/CMakeFiles/ArticulationPoint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "ArticulationPoint"
+  "ArticulationPoint.pdb"
+  "CMakeFiles/ArticulationPoint.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArticulationPoint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
